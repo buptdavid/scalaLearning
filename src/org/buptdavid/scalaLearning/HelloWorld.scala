912a48f778855daa3247c6f4b6470f0d318a54e3
luweijie [路卫杰] <luweijie@xiaomi.com>
2015-06-24 13:54:04 +0800

    the first scala

---
+++ src/org/buptdavid/scalaLearning/HelloWorld.scala
@@ -0,0 +1,11 @@
+package org.buptdavid.scalaLearning
+
+/**
+ * Created by luweijie on 15-6-19.
+ */
+object HelloWorld {
+  def main (args: Array[String]): Unit = {
+  print("Hello World")
+  }
+
+}
